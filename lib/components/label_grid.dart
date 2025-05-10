b5b42a68a5237bcfb73330a5fbef0a4fcc5f9e98
oJaciel <jacielhbarichellO@gmail.com>
2025-05-09 21:17:55 -0300

    Finalização LabelGrid
    Finalizada a modelagem da grid que irá aparecer na tela inicial, mostrando todas as labels cadastradas.

--- lib/components/label_grid.dart
+++ lib/components/label_grid.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:label_craft/components/label_grid_item.dart';
+import 'package:label_craft/models/label.dart';
+import 'package:label_craft/models/label_provider.dart';
+import 'package:provider/provider.dart';
 
 class LabelGrid extends StatelessWidget {
   const LabelGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<LabelProvider>(context);
+
+    final List<Label> loadedLabels = provider.labels;
+
     return GridView.builder(
-      itemCount: 10, // Substitua com a quantidade real de itens
+      itemCount: loadedLabels.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => Card(
-        child: LabelGridItem(),
-      ),
+      itemBuilder: (ctx, i) => LabelGridItem(loadedLabels[i]),
     );
   }
 }
--- lib/components/label_grid_item.dart
+++ lib/components/label_grid_item.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:label_craft/models/label.dart';
 
 class LabelGridItem extends StatelessWidget {
-  const LabelGridItem({super.key});
+  const LabelGridItem(this.label, {super.key});
+
+  final Label label;
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Center(child: Text('Etiqueta')));
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          border: Border.all(color: Colors.grey.shade400),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // alinha à esquerda
+          children: [
+            Text(
+              label.name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(label.hasWeight ? 'Peso: ${label.weight}' : 'Peso:______'),
+            Text(label.hasPrice ? 'Preço: R\$ ${label.price}' : 'Preço: R\$ ______'),
+            if (label.hasFab)
+              const Text('Data de fabricação: ____/____/_____', style: TextStyle(fontSize: 13),),
+            if (label.hasExpDate)
+              const Text('Validade: 02 meses (sob congelamento)', style: TextStyle(fontSize: 13),),
+          ],
+        ),
+      ),
+    );
   }
 }
